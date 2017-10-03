aaf714ea35e648556dc40ddfeaddf1b278ae0931
Julia Dias <juliamzdias@gmail.com>
2017-10-02 22:47:39 -0300

    Scheduler funcionando :)

--- src/main.c
+++ src/main.c
@@ -2,12 +2,40 @@
 
 int main() {
   int a[5];
-  int n;
+  int n, flag=0;
 
   scanf("%d %d %d %d %d %d", &(a[0]), &(a[1]), &(a[2]), &(a[3]), &(a[4]), &n);
 
-  for (int i=0; i<n; i++) {
+  for (int i=1; i<=n; i++) {
     /* Imprime resultado da n-esima iteracao do scheduler */
+    flag=0;
+
+    if (a[0]==i || i%a[0]==0 ){
+      printf("0");
+    } else flag++;
+
+    if (a[1]==i || i%a[1]==0 ){
+      printf("1");
+    } else flag++;
+
+    if (a[2]==i || i%a[2]==0 ){
+      printf("2");
+    } else flag++;
+
+    if (a[3]==i || i%a[3]==0 ){
+      printf("3");
+    } else flag++;
+
+    if (a[4]==i || i%a[4]==0 ){
+      printf("4");
+    } else flag++;
+
+    if(flag==5){
+      printf("-");
+    }
+
+    printf("\n");
+
   }
 
   return 0;
